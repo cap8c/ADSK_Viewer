7ae09d17b954d57fe0286943d1ba36fce105f598
Cody Pennetti <53183444+cap8c@users.noreply.github.com>
2024-08-14 15:59:34 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var viewer;
 var options = {
     env: 'AutodeskProduction',
     accessToken: 'YOUR_ACCESS_TOKEN',  // Placeholder to be replaced by GitHub Actions
-    api: 'derivativeV2'  // Use 'derivativeV2_EU' for models uploaded in Europe
+    api: 'derivativeV2'
 };
-var documentId = 'urn:YOUR_URN_ID';  // Placeholder for URN
+var documentId = 'urn:dXJuOmFkc2sud2lwLmZvcmdlLmFwaS5jb20vc2FtcGxlLnJ2dA';  // Sample model URN
 
 Autodesk.Viewing.Initializer(options, function onInitialized(){
     Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
